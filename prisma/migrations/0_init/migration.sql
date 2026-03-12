efb899c985d10394bcf75fca208b470aec96ece9
Necrometal <gisenael.dev44@gmail.com>
2026-03-12 15:56:35 +0300

    config: change migration reset setup

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -21,3 +21,4 @@ CREATE TABLE `Role` (
 
 -- AddForeignKey
 ALTER TABLE `User` ADD CONSTRAINT `User_roleId_fkey` FOREIGN KEY (`roleId`) REFERENCES `Role`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
