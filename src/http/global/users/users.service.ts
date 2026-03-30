b0b585c6cd86c42a44997a496b2d9dc922c8bf31
Necrometal <gisenael.dev44@gmail.com>
2026-03-30 13:08:20 +0300

    feat: create validation code to registred user

--- src/http/global/users/users.service.ts
+++ src/http/global/users/users.service.ts
@@ -1,9 +1,13 @@
 import { PrismaService } from '@/prisma.service';
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/user.dto';
-import { HelpersService } from '@/helpers/helpers.service';
 import { UserDataSimple } from '@/http/global/fragments/user';
 import type { User } from '@/http/model';
+import { env } from 'prisma/config';
+import { addMinute } from '@formkit/tempo';
+import { CODE_DURATION } from '@/constantes/global';
+import { hash } from '@/helpers/helpers.text';
+import { generateNumber } from '@/helpers/helpers.number';
 
 @Injectable()
 export class UsersService {
@@ -12,10 +16,16 @@ export class UsersService {
     const result: User = await this.prisma.user.create({
       data: {
         email: user.email,
-        password: await HelpersService.hash(user.password),
+        password: await hash(user.password),
         role: {
           connect: {
             id: user.roleId,
+          },
+        },
+        validation: {
+          create: {
+            expiredAt: addMinute(new Date(), parseInt(env(CODE_DURATION))),
+            code: generateNumber(6, true) as string,
           },
         },
       },
