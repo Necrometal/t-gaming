78d7bc3c58ac891b28051baed7d0fca31f2b97a9
Necrometal <gisenael.dev44@gmail.com>
2026-04-02 09:21:31 +0300

    fix: fix i18n config

---
+++ src/configs/i18n/index.ts
@@ -0,0 +1,23 @@
+import { ConfigService } from '@nestjs/config';
+import { AcceptLanguageResolver, HeaderResolver, I18nModule, QueryResolver } from 'nestjs-i18n';
+import * as path from 'path';
+import { env } from 'prisma/config';
+
+const i18nConfig = () => {
+  return I18nModule.forRootAsync({
+    useFactory: () => ({
+      fallbackLanguage: env('FALLBACK_LANGUAGE'),
+      loaderOptions: {
+        path: path.join(__dirname, '/i18n/'),
+        watch: true,
+      },
+    }),
+    resolvers: [
+      { use: QueryResolver, options: ['lang'] },
+      AcceptLanguageResolver,
+      new HeaderResolver(['x-lang']),
+    ],
+  });
+};
+
+export default i18nConfig;
